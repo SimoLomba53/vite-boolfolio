8fe365ebd6a395c4643ce983a0a22a4ec9ceda3c
SimoLomba53 <simolomba253@gmail.com>
2023-04-26 15:33:09 +0200

    ordine homepage

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,24 @@
 <script>
-import axios from "axios";
 import AppHeader from "./components/AppHeader.vue";
-import ProjectList from "./components/ProjectList.vue";
+import HomePage from "./pages/HomePage.vue";
+
 
 export default {
   data() {
     return {
       title: "BOOLFOLIO",
-      projs: [],
     };
   },
-  components: { AppHeader, ProjectList },
+  components: { AppHeader,HomePage },
 
-  methods: {
-    fetchProjs() {
-      axios.get("http://127.0.0.1:8000/api/projs").then((response) => {
-        this.projs = response.data;
-      });
-    },
-  },
-
-  created() {
-    this.fetchProjs();
-  },
+  
 };
 </script>
 
 <template>
   <AppHeader />
   <h1 class="text-center p-2">{{ title }}</h1>
-  <ProjectList :projs="projs" />
+  <HomePage/>
 </template>
 
 <style lang="scss" scoped></style>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,33 @@
+<script>
+
+import axios from "axios";
+import ProjectList from "../components/ProjectList.vue";
+
+export default {
+  data() {
+    return {
+      projs: [],
+    };
+  },
+
+  components: { ProjectList },
+
+  methods: {
+    fetchProjs() {
+      axios.get("http://127.0.0.1:8000/api/projs").then((response) => {
+        this.projs = response.data;
+      });
+    },
+  },
+
+  created() {
+    this.fetchProjs();
+  },
+}
+</script>
+
+<template>
+  <ProjectList :projs="projs" />
+</template>
+
+<style lang="scss" scoped></style>
